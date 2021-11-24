4e4e7e4b25b8ce9627175690a5f7892ebee7a5f8
Shayan Nasrabadi <williams.ca001@gmail.com>
2021-11-24 19:58:31 +0330

    Server Info Fixed

--- commands/5-server-info.js
+++ commands/5-server-info.js
@@ -1,8 +1,4 @@
-// ! Iterable
-
 const { magenta, white } = require("chalk");
-const prompt = require("prompt-sync")();
-const fs = require("fs");
 
 module.exports = {
   name: "server-info",
@@ -12,23 +8,25 @@ module.exports = {
     if (!findGuild)
       throw new Error("Insert correct guild id (Guild not found)");
 
-    const information = {
-      ID: findGuild.id || "Nothing",
-      "Description ": findGuild.description || "Nothing",
-      //   Icon: findGuild.icon({ dynamic: true, size: 1024 }) || "Nothing",
-      Region: findGuild.region || "Nothing",
-      "Member Count": findGuild.memberCount || "Nothing",
-      "AFK TimeOut": findGuild.afkTimeout || "Nothing",
-      "AFK Channel ID": findGuild.afkChannelID || "Nothing",
-      "Verification Level": findGuild.verificationLevel || "Nothing",
-      "Maximum Members": findGuild.maximumMembers || "Nothing",
-      Banner: findGuild.banner || "Nothing",
-      "Rules Channel ID": findGuild.rulesChannelID || "Nothing",
-      "Owner ID": findGuild.ownerID || "Nothing",
-    };
+    const information = [
+      { name: "ID", value: findGuild.id || NaN },
+      { name: "Description", value: findGuild.description || NaN },
+      { name: "Member Count", value: findGuild.memberCount || NaN },
+      { name: "AFK TimeOut", value: findGuild.afkTimeout || NaN },
+      { name: "AFK Channel ID", value: findGuild.afkChannelID || NaN },
+      {
+        name: "Verification Level",
+        value: findGuild.verificationLevel || NaN,
+      },
+      { name: "Maximum Members", value: findGuild.maximumMembers || NaN },
+      { name: "Banner", value: findGuild.banner || NaN },
+      {
+        name: "Rules Channel ID",
+        value: findGuild.rulesChannelID || NaN,
+      },
+      { name: "Owner ID", value: findGuild.ownerID || NaN },
+    ];
 
-    for (const [i, v] of information) {
-      console.log(i, v);
-    }
+    console.table(information);
   },
 };
